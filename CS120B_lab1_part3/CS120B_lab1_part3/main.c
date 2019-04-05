616200e58eb877008202a3802f0d437dbec6fff9
Bon Won Koo <33435614+bdubs006@users.noreply.github.com>
2019-04-05 12:09:14 -0700

    Update main.c

--- CS120B_lab1_part3/CS120B_lab1_part3/main.c
+++ CS120B_lab1_part3/CS120B_lab1_part3/main.c
@@ -10,29 +10,29 @@
 
 int main(void)
 {
-    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-    DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-    unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-    unsigned char tmpC = 0x00;
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	unsigned char tmpC = 0x00;
 	unsigned char tmpD = 0x00;
 	unsigned char cntavail = 0x00;
 	
 	while(1)
-    {
-	    tmpA = PINA & 0x01; //sets tmpA to bit position 0, 1, 2, 3
-	    tmpB = PINA & 0x02;
-	    tmpC = PINA & 0x04;
-	    tmpD = PINA & 0x08;
+	{
+	tmpA = PINA & 0x01; //sets tmpA to bit position 0, 1, 2, 3
+	tmpB = PINA & 0x02;
+	tmpC = PINA & 0x04;
+	tmpD = PINA & 0x08;
 		
 	    
-	    tmpB = tmpB >> 1;
-	    tmpC = tmpC >> 2;
-	    tmpD = tmpD >> 3;
+	tmpB = tmpB >> 1;
+	tmpC = tmpC >> 2;
+	tmpD = tmpD >> 3;
 		
-		cntavail = 0x04 - (tmpA+tmpB+tmpC+tmpD);
+	cntavail = 0x04 - (tmpA+tmpB+tmpC+tmpD);
 	    
-	    if (cntavail == 0) {
+		if (cntavail == 0) {
 			PORTC = 0x80;
 		}
 		else {
@@ -45,7 +45,7 @@ int main(void)
 			PORTC = cntavail;			//output the inversion (which are the spots open), which is what we want
 		}
 			
-    }
-    return 0;
+	}
+	return 0;
 }
 
