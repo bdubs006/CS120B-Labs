da1b8f7b9f6be6a2b8caf0d3c18a955481be7ff7
Bon Won Koo <33435614+bdubs006@users.noreply.github.com>
2019-04-05 12:02:10 -0700

    CS120B_lab1_part2
    part2

--- CS120B_lab1_part1/CS120B_lab1_part1/main.c
+++ CS120B_lab1_part1/CS120B_lab1_part1/main.c
@@ -10,23 +10,23 @@
 
 int main(void)
 {
-    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00;
-    unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-    while(1)
+	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	while(1)
     {
-	    // 1) Read input
-	    tmpA = PINA & 0x01;
-		tmpB = PINA & 0x02;
-	    // 2) Perform computation
-	    // if PA0 is 1, set PB1PB0 = 01, else = 10
-	    if (tmpA == 0x01 && tmpB == 0x00) { // True if PA0 is 1
-		    PORTB = 0x01;
-	    }
-	    else {
-		    PORTB = 0x00;
-	    }
+	// 1) Read input
+	tmpA = PINA & 0x01;
+	tmpB = PINA & 0x02;
+	// 2) Perform computation
+	// if PA0 is 1, set PB1PB0 = 01, else = 10
+		if (tmpA == 0x01 && tmpB == 0x00) { // True if PA0 is 1
+			PORTB = 0x01;
+		}
+		else {
+			PORTB = 0x00;
+		}
 	    
     }
     return 0;
--- CS120B_lab1_part2/CS120B_lab_part2/main.c
+++ CS120B_lab1_part2/CS120B_lab_part2/main.c
@@ -10,19 +10,19 @@
 
 int main(void)
 {
-    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-    DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
     
 	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-    unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
-    unsigned char tmpC = 0x00;
+	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	unsigned char tmpC = 0x00;
 	unsigned char tmpD = 0x00;
 	unsigned char cntavail = 0x00;
 	
 	while(1)
-    {
-	    // 1) Read input
-	    tmpA = PINA & 0x01; //sets tmpA to bit position 0, 1, 2, 3
+	{
+		// 1) Read input
+		tmpA = PINA & 0x01; //sets tmpA to bit position 0, 1, 2, 3
 		tmpB = PINA & 0x02;
 		tmpC = PINA & 0x04;
 		tmpD = PINA & 0x08;
@@ -35,7 +35,7 @@ int main(void)
 		PORTC = cntavail;
 	    
 	
-    }
+	}
     return 0;
 }
 
