9b9101287ab14f5e3cdc485e39124992daa10805
Bon Won Koo <33435614+bdubs006@users.noreply.github.com>
2019-04-09 12:20:19 -0700

    Update main.c

--- CS120B_lab3_part4/CS120B_lab3_part4/main.c
+++ CS120B_lab3_part4/CS120B_lab3_part4/main.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
-    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-    DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs,
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs,
 	DDRC = 0xFF; PORTC = 0x00;
 	unsigned char tmpB = 0x00;
 	unsigned char tmpC = 0x00;
