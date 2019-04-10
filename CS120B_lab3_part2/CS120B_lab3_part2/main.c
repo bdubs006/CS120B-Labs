2a4746b584bbb9e08f6f27341d35858dcf6812ec
Bon Won Koo <33435614+bdubs006@users.noreply.github.com>
2019-04-09 19:19:37 -0700

    lab3 part 2

---
+++ CS120B_lab3_part2/CS120B_lab3_part2/main.c
@@ -0,0 +1,81 @@
+/*	Partner 1 Name & E-mail: Bon Won Koo bkoo006@ucr.edu
+ *	Partner 2 Name & E-mail: Vinh-Trung Trinh vtrin003@ucr.edu
+ *	Lab Section: 028
+ *	Assignment: Lab   Exercise 
+ *	Exercise Description: [optional - include for your own benefit]
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	unsigned char fuellvl = 0x00;
+	unsigned char sensorLEDS = 0x00;
+    /* Replace with your application code */
+    while (1) 
+    {
+		fuellvl = PINA; //get the input
+		switch(fuellvl){
+			case 0:
+				sensorLEDS = 0x40; //10 0000
+				break;
+			case 1:
+				sensorLEDS = 0x60; //still needs pc6 to be included, 110 0000
+				break;			
+			case 2:
+				sensorLEDS = 0x60;
+				break;		
+			case 3:
+				sensorLEDS = 0x70; //still need pc6
+				break;		
+			case 4:
+				sensorLEDS = 0x70;
+				break;		
+			case 5:
+				sensorLEDS = 0x38;
+				break;		
+			case 6:
+				sensorLEDS = 0x38;
+				break;		
+			case 7:
+				sensorLEDS = 0x3C;
+				break;		
+			case 8:
+				sensorLEDS = 0x3C;
+				break;	
+			case 9:
+				sensorLEDS = 0x3C;
+				break;	
+			case 10:
+				sensorLEDS = 0x3E;
+				break;	
+			case 11:
+				sensorLEDS = 0x3E;
+				break;
+			case 12:
+				sensorLEDS = 0x3E; //111 110
+				break;			
+			case 13:
+				sensorLEDS = 0x3F;
+				break;			
+			case 14:
+				sensorLEDS = 0x3F;
+				break;	
+			case 15:
+				sensorLEDS = 0x3F; //111 111
+				break;	
+		}
+		PORTC = sensorLEDS; //how you output to the display, assign the output to your variablw
+		sensorLEDS = 0x00; //reset variables
+		fuellvl = 0x00; //reset variables
+    }
+	return 1;
+}
+
+
